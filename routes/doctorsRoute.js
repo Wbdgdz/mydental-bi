4078a04cc9d762c06f0be0cdbebbc5ae72db16aa
qurnt1 <quentin.chabot@etu.univ-poitiers.fr>
2025-12-10 14:01:32 +0100

    fix: ajout du not in (User,ADMIN,Technologie)

--- routes/doctorsRoute.js
+++ routes/doctorsRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 module.exports = (connection) => {
   router.get('/', (req, res) => {
     const query = `
-    SELECT id, firstName, lastName FROM user WHERE user.discr = "Doctor" and user.enabled=1;
+    SELECT id, firstName, lastName FROM user WHERE user.discr = "Doctor" and user.enabled=1 and firstname NOT IN ("User","ADMIN","Technologie");
     `;
     connection.query(query, (error, results) => {
       if (error) throw error;
